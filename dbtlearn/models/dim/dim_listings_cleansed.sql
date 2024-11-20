d6dd343e6843597a84b1472295cc664bb908cf9c
Tamas Papp <tamaspapp84@gmail.com>
2024-11-20 11:55:16 +0000

    add content

---
+++ dbtlearn/models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,19 @@
+WITH src_listings as (
+    select * from {{ ref("src_listings") }}
+)
+
+select 
+listing_id,
+listing_name,
+room_type,
+CASE minimum_nights = 0
+THEN 1
+ELSE minimum_nights
+END as minimum_nights,
+host_id,
+replace(
+    price_str,
+    '$') :: NUMBER(10,2) as price,
+created_at,
+updated_at
+from src_listings
