41b9f35c3fb64aec5b6e1b2776c7c5c37db3fc79
Tamas Papp <tamaspapp84@gmail.com>
2024-11-20 13:10:01 +0000

    fct stuff

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -1,2 +1,16 @@
-WITH src_reviews AS (
-select * from {{ ref("") }}
+{{
+config(materialized='incremental',
+                    on_schema_change='fail')
+}}
+
+WITH src_reviews as (
+    select * from {{ ref("src_reviews") }}
+)
+
+select 
+*
+from src_reviews
+where review_text is not null
+{% if is_incremental() %}
+and review_date > (select max(review_date) from {{ this }})
+{% endif %}
